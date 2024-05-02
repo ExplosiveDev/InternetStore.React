17a6ba858f3dcb24b056a250b48489ddc40e39e1
Vlad <Vlad@HOME-PC>
2024-05-03 00:10:55 +0300

    Add EncryptedObject in LocalStorage

--- products-catalog/src/components/hooks/auth.hook.ts
+++ products-catalog/src/components/hooks/auth.hook.ts
@@ -1,7 +1,9 @@
 import { useCallback, useState, useEffect, useId } from "react"
+import {secureLocalStorage} from "./secureLocalStorage.hook"
 import User from "../../Models/User";
 import Basket from "../../Models/Basket";
 import ProductInBasket from "../../Models/ProductInBasket";
+
 
 const storageName = 'userData';
 const storageCartName = 'userCart';
@@ -15,13 +17,14 @@ const initState:User = {
 };
 
 export const useAuth = () => {
-
     const [token, setToken] = useState<string | null>();
     const [user, setUser] = useState<User | null>();
     const [ProductInBasket, setProducts] = useState<ProductInBasket[] | null>();
+    const {secretKey, saveEncryptedObject, getDecryptedObject} = secureLocalStorage()
 
     const changeCount = useCallback((operation: string, productId: string) => {
-        const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
+        const products = getDecryptedObject(storageCartName,secretKey) as ProductInBasket[];
+        // const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
 
         const productIndex = products.findIndex(product => product.id === productId);
         if (productIndex !== -1) {
@@ -33,7 +36,8 @@ export const useAuth = () => {
             }
 
             setProducts(products);
-            localStorage.setItem(storageCartName, JSON.stringify(products));
+            saveEncryptedObject(storageCartName,products,secretKey);
+            // localStorage.setItem(storageCartName, JSON.stringify(products));
 
         } else {
             console.log("Product not found in the basket.");
@@ -41,60 +45,70 @@ export const useAuth = () => {
     }, [])
 
     const deleteProduct = useCallback((prodId: string) => {
-        const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
+        const products = getDecryptedObject(storageCartName,secretKey) as ProductInBasket[];
+        // const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
         const index = products.findIndex((item) => item.id === prodId);
         if (index !== -1) {
             products.splice(index, 1);
             setProducts(products);
-            localStorage.setItem(storageCartName, JSON.stringify(products));
+            saveEncryptedObject(storageCartName,products,secretKey);
+            // localStorage.setItem(storageCartName, JSON.stringify(products));
         }
     }, [])
 
     const isLocalCartEmpty = useCallback((): boolean => {
-        let products = [];
         try {
-            const data = localStorage.getItem(storageCartName);
-
-            if (data) {
-                products = JSON.parse(data);
+            const data = getDecryptedObject(storageCartName, secretKey);
+    
+            if (Array.isArray(data) && data.length === 0) {
+                return true;
             }
-
+    
+            if (data === null || data === undefined) {
+                return true;
+            }
+    
+            return false; 
+    
         } catch (error) {
             console.error('Помилка під час читання з localStorage:', error);
+            return true; 
         }
-
-        if (products == null || products.length == 0) {
-            return true;
-        }
-        return false;
-
     }, []);
+    
 
     const addProductInCart = useCallback((product: ProductInBasket) => {
         if (isLocalCartEmpty()) {
             const products: ProductInBasket[] = [];
             products.push(product)
-            localStorage.setItem(storageCartName, JSON.stringify(products));
+            saveEncryptedObject(storageCartName,products,secretKey);
+            // localStorage.setItem(storageCartName, JSON.stringify(products));
         }
         else {
-            const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
+            const products = getDecryptedObject(storageCartName,secretKey) as ProductInBasket[];
+            // const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
             products.push(product);
-            const data = localStorage.getItem(storageCartName);
+            const data = getDecryptedObject(storageCartName,secretKey);
+            // const data = localStorage.getItem(storageCartName);
             if (data) {
-                localStorage.setItem(storageCartName, JSON.stringify([]))
+                saveEncryptedObject(storageCartName,[],secretKey);
+                // localStorage.setItem(storageCartName, JSON.stringify([]))
             }
-            localStorage.setItem(storageCartName, JSON.stringify(products));
+            saveEncryptedObject(storageCartName,products,secretKey);
+            // localStorage.setItem(storageCartName, JSON.stringify(products));
         }
     }, [])
 
     const setUserBasketFromLocal = useCallback(() => {
-        const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
+        const products = getDecryptedObject(storageCartName,secretKey) as ProductInBasket[];
+        // const products = JSON.parse(localStorage.getItem(storageCartName)!) as ProductInBasket[];
         setProducts(products);
     }, [])
 
     const setUserBasketFromBase = useCallback((Basket: Basket) => {
         setProducts(Basket.products);
-        localStorage.setItem(storageCartName, JSON.stringify(Basket.products))
+        saveEncryptedObject(storageCartName,Basket.products,secretKey);
+        // localStorage.setItem(storageCartName, JSON.stringify(Basket.products))
     }, [])
 
 
@@ -103,9 +117,10 @@ export const useAuth = () => {
         setToken(jwtToken);
         setUser(user);
 
-        localStorage.setItem(storageName, JSON.stringify({
-            user: user, token: jwtToken
-        }))
+        saveEncryptedObject(storageName,{user: user, token: jwtToken},secretKey);
+        // localStorage.setItem(storageName, JSON.stringify({
+        //     user: user, token: jwtToken
+        // }))
     }, [])
 
     const clearCart = useCallback(() => {
@@ -121,7 +136,8 @@ export const useAuth = () => {
     }, [])
 
     useEffect(() => {
-        const data = JSON.parse(localStorage.getItem(storageName)!);
+        const data = getDecryptedObject(storageName,secretKey)
+        // const data = JSON.parse(localStorage.getItem(storageName)!);
 
         if (data && data.token) {
             login(data.token, data.user)
